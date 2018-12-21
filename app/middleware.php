d760663abe924ca5add0fdd4a9a9e8a1b129f575
devel-srv <dev@astrolabio.fccn.pt>
2018-12-21 14:18:39 +0000

    changes on middleware handler

--- app/middleware.php
+++ app/middleware.php
@@ -1,15 +1,11 @@
 <?php
 /*
- * Loads middleware into Slim
+ * Loads global middlewares into Slim
  *
 */
 
 use \Fccn\Lib\FileLogger;
 use \Fccn\Lib\SiteConfig;
-
-//Add locale middleware
-$container = $app->getContainer();
-$app->add(new Fccn\WebComponents\LocaleMiddleware($container['locale']));
 
 //--- include other middlewares defined in middlewares folder
 foreach (glob("middlewares/*.php") as $filename) {
